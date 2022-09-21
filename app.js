16d25eb35d252f26011cc9c2c499bfb1462d7b10
Héctor González Pereda <hgonza02@ucm.es>
2022-09-21 21:29:23 +0200

    fs.module

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 //const saludar = require ("./saludar");
 const {saludar, saludarHelloWorld} = require('./saludar.js');
+const fs = require ('fs');
 
-console.log(saludar("Andriu"));
-console.log(saludarHelloWorld());
+//fs Module
+/*fs.readFile('text.html','UTF-8',(err, data1) => {
+    if(err){
+        throw err
+    }
+    console.log(data1);
+});*/
+/*fs.rename('text.html', 'newname.html', (err) =>{
+    if (err){
+    throw err
+    }
+    console.log('name changed')
+});*/
+
+fs.appendFile('text.html', '<p>data to add file</p>', (err) => {
+    if (err){
+        throw err;
+    }
+    console.log ('Data add file saccesfully')
+});
+
+//fs.writeFile...
+
+//console.log(saludar("Andriu"));
+//console.log(saludarHelloWorld());
